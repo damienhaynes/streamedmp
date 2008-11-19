8a945612a78cf064ca94e97bcc20ef53ff232dc1
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2008-11-19 00:33:46 +0000

    * Commit source to StreamedMP Editor. Some cleanup of source (renaming of controls etc) * Loads the StreamedMP skin by default now if it exists in the MediaPortal directory * Some minor UI adjustments (resizing)

---
+++ trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StreamedMP Editor")]
+[assembly: AssemblyDescription("A Basic Home menu editor for the MediaPortal StreamedMP skin\r\n\r\nAuthors:\r\n   jburnette\r\n   Sambal   Mattsk88")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StreamedMP Menu Editor")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93166a88-779e-4126-a864-6c2cbcf9a23c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
