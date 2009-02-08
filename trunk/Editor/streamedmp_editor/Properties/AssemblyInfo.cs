84a17944bf343a2379c3d753d6799bb538277f40
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2009-02-08 01:34:38 +0000

    * Updated version to v0.5.361

--- trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
+++ trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.298.0")]
-[assembly: AssemblyFileVersion("0.4.298.0")]
+[assembly: AssemblyVersion("0.5.361.0")]
+[assembly: AssemblyFileVersion("0.5.361.0")]
