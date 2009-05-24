8a2cd23623613554e2194704541b7b4c0034866f
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2009-05-24 00:34:54 +0000

    Issue 92: Launch My Lyrics from Hidden menu in Music playlist

--- trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
+++ trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.445.0")]
-[assembly: AssemblyFileVersion("0.8.445.0")]
+[assembly: AssemblyVersion("0.9.463.0")]
+[assembly: AssemblyFileVersion("0.9.463.0")]
