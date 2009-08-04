d8c7c3cdba173f0567f40d9554c2ba743f5f81aa
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2009-08-04 08:27:38 +0000

    Issue 160: Allow Configurable List Colours e.g. (Watched|Default|Unavailable|GroupHeaders)

---
+++ trunk/MSI/StreamedMP/SetupHelper/FacadeProperties/Colors.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FacadeProperties {
+    public class Colors {
+
+        private ListColorDialog ColorDialog = new ListColorDialog();
+
+        public bool ShowColors() {
+            DialogResult result = ColorDialog.ShowDialog();
+            if (result == DialogResult.OK) {
+                return true;
+            }
+            return false;
+        }
+
+        #region Public Get Colour methods
+        
+        public string GetTextColor() {
+            return ColorDialog.ColorText;
+        }
+
+        public string GetText2Color() {
+            return ColorDialog.ColorText2;
+        }
+
+        public string GetText3Color() {
+            return ColorDialog.ColorText3;
+        }
+
+        public string GetWatchedColor() {
+            return ColorDialog.ColorWatched;
+        }
+
+        public string GetRemoteColor() {
+            return ColorDialog.ColorRemote;
+        }
+
+        #endregion
+
+        #region Public Set Colour methods
+
+        public void SetTextColor(string col) {
+            ColorDialog.ColorText = col;
+        }
+
+        public void SetText2Color(string col) {
+            ColorDialog.ColorText2 = col;
+        }
+
+        public void SetText3Color(string col) {
+            ColorDialog.ColorText3 = col;
+        }
+
+        public void SetWatchedColor(string col) {
+            ColorDialog.ColorWatched = col;
+        }
+
+        public void SetRemoteColor(string col) {
+            ColorDialog.ColorRemote = col;
+        }
+
+        #endregion
+
+    }
+}
---
+++ trunk/MSI/StreamedMP/SetupHelper/FacadeProperties/Forms/ListColorDialog.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FacadeProperties {
+    public partial class ListColorDialog : Form {
+
+        #region Color Properties
+        public string ColorText { get; set; }
+        public string ColorWatched { get; set; }
+        public string ColorRemote { get; set; }
+        public string ColorText2 { get; set; }
+        public string ColorText3 { get; set; }
+        #endregion
+
+        #region Constructor
+        public ListColorDialog() {
+            InitializeComponent();            
+        }
+        #endregion        
+
+        #region Colour Dialog Invoke
+        private void btnText_Click(object sender, EventArgs e) {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) {              
+                txtText.ForeColor = colorDialog.Color;
+                txtText.Text = (colorDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                ColorText = txtText.Text;
+            }
+        }
+
+        private void btnWatched_Click(object sender, EventArgs e) {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) {                
+                txtWatched.ForeColor = colorDialog.Color;
+                txtWatched.Text = (colorDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                ColorWatched = txtWatched.Text;
+            }
+        }
+
+        private void btnRemote_Click(object sender, EventArgs e) {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) {                
+                txtRemote.ForeColor = colorDialog.Color;
+                txtRemote.Text = (colorDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                ColorRemote = txtRemote.Text;
+            }
+        }
+
+        private void btnText2_Click(object sender, EventArgs e) {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) {                
+                txtText2.ForeColor = colorDialog.Color;
+                txtText2.Text = (colorDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                ColorText2 = txtText2.Text;
+            }
+        }
+
+        private void btnText3_Click(object sender, EventArgs e) {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) {               
+                txtText3.ForeColor = colorDialog.Color;
+                txtText3.Text = (colorDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                ColorText3 = txtText3.Text;
+            }
+        }                
+        #endregion
+
+        protected override void OnLoad(EventArgs e) {            
+            UpdateColors();
+            base.OnLoad(e);
+        }
+
+        private Color ColorInvert(Color colorIn) {
+            return Color.FromArgb(colorIn.A, Color.White.R - colorIn.R,
+                   Color.White.G - colorIn.G, Color.White.B - colorIn.B);
+        }
+
+        private Color ColorFromRGB(string RGB)
+        {
+            if (RGB.Length != 6)
+                return System.Drawing.Color.FromArgb(255, 255, 255);
+
+            byte R = ColorTranslator.FromHtml("#" + RGB).R;
+            byte G = ColorTranslator.FromHtml("#" + RGB).G;
+            byte B = ColorTranslator.FromHtml("#" + RGB).B;
+
+            return System.Drawing.Color.FromArgb(int.Parse(R.ToString()),
+                                                 int.Parse(G.ToString()),
+                                                 int.Parse(B.ToString()));
+        }
+
+        private void btnOK_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void UpdateColors() {            
+            txtText.Text = ColorText;            
+            txtText.ForeColor = ColorFromRGB(ColorText);
+            
+            txtText2.Text = ColorText2;
+            txtText2.ForeColor = ColorFromRGB(ColorText2);
+
+            txtText3.Text = ColorText3;
+            txtText3.ForeColor = ColorFromRGB(ColorText3);
+
+            txtWatched.Text = ColorWatched;
+            txtWatched.ForeColor = ColorFromRGB(ColorWatched);
+
+            txtRemote.Text = ColorRemote;
+            txtRemote.ForeColor = ColorFromRGB(ColorRemote);
+        }
+    }
+}
