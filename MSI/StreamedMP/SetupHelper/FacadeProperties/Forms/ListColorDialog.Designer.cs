4095d9da2c75788897a2619d45262d06f294f870
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2009-08-04 08:27:38 +0000

    Issue 160: Allow Configurable List Colours e.g. (Watched|Default|Unavailable|GroupHeaders)

---
+++ MSI/StreamedMP/SetupHelper/FacadeProperties/Forms/ListColorDialog.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace FacadeProperties {
+    partial class ListColorDialog {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListColorDialog));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnText3 = new System.Windows.Forms.Button();
+            this.txtText3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnText2 = new System.Windows.Forms.Button();
+            this.txtText2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRemote = new System.Windows.Forms.Button();
+            this.txtRemote = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnWatched = new System.Windows.Forms.Button();
+            this.txtWatched = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnText = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(123, 170);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(62, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnText3);
+            this.groupBox1.Controls.Add(this.txtText3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnText2);
+            this.groupBox1.Controls.Add(this.txtText2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnRemote);
+            this.groupBox1.Controls.Add(this.txtRemote);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnWatched);
+            this.groupBox1.Controls.Add(this.txtWatched);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnText);
+            this.groupBox1.Controls.Add(this.txtText);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 161);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose Colours:";
+            // 
+            // btnText3
+            // 
+            this.btnText3.Location = new System.Drawing.Point(130, 125);
+            this.btnText3.Name = "btnText3";
+            this.btnText3.Size = new System.Drawing.Size(27, 22);
+            this.btnText3.TabIndex = 14;
+            this.btnText3.Text = "...";
+            this.toolTip.SetToolTip(this.btnText3, resources.GetString("btnText3.ToolTip"));
+            this.btnText3.UseVisualStyleBackColor = true;
+            this.btnText3.Click += new System.EventHandler(this.btnText3_Click);
+            // 
+            // txtText3
+            // 
+            this.txtText3.BackColor = System.Drawing.Color.Black;
+            this.txtText3.ForeColor = System.Drawing.Color.White;
+            this.txtText3.Location = new System.Drawing.Point(75, 127);
+            this.txtText3.Name = "txtText3";
+            this.txtText3.Size = new System.Drawing.Size(49, 20);
+            this.txtText3.TabIndex = 13;
+            this.txtText3.Text = "FFFFFF";
+            this.txtText3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Text 3:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnText2
+            // 
+            this.btnText2.Location = new System.Drawing.Point(130, 99);
+            this.btnText2.Name = "btnText2";
+            this.btnText2.Size = new System.Drawing.Size(27, 22);
+            this.btnText2.TabIndex = 11;
+            this.btnText2.Text = "...";
+            this.toolTip.SetToolTip(this.btnText2, "Choose the colour that represents text2 in the list facade, this is typically ext" +
+                    "ra metadata\r\nfor the selected item e.g. Runtime, Aired Date, Episode counts...");
+            this.btnText2.UseVisualStyleBackColor = true;
+            this.btnText2.Click += new System.EventHandler(this.btnText2_Click);
+            // 
+            // txtText2
+            // 
+            this.txtText2.BackColor = System.Drawing.Color.Black;
+            this.txtText2.ForeColor = System.Drawing.Color.White;
+            this.txtText2.Location = new System.Drawing.Point(75, 101);
+            this.txtText2.Name = "txtText2";
+            this.txtText2.Size = new System.Drawing.Size(49, 20);
+            this.txtText2.TabIndex = 10;
+            this.txtText2.Text = "FFFFFF";
+            this.txtText2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Text 2:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnRemote
+            // 
+            this.btnRemote.Location = new System.Drawing.Point(130, 73);
+            this.btnRemote.Name = "btnRemote";
+            this.btnRemote.Size = new System.Drawing.Size(27, 22);
+            this.btnRemote.TabIndex = 8;
+            this.btnRemote.Text = "...";
+            this.toolTip.SetToolTip(this.btnRemote, resources.GetString("btnRemote.ToolTip"));
+            this.btnRemote.UseVisualStyleBackColor = true;
+            this.btnRemote.Click += new System.EventHandler(this.btnRemote_Click);
+            // 
+            // txtRemote
+            // 
+            this.txtRemote.BackColor = System.Drawing.Color.Black;
+            this.txtRemote.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtRemote.Location = new System.Drawing.Point(75, 75);
+            this.txtRemote.Name = "txtRemote";
+            this.txtRemote.Size = new System.Drawing.Size(49, 20);
+            this.txtRemote.TabIndex = 7;
+            this.txtRemote.Text = "FFA075";
+            this.txtRemote.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Remote:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnWatched
+            // 
+            this.btnWatched.Location = new System.Drawing.Point(130, 47);
+            this.btnWatched.Name = "btnWatched";
+            this.btnWatched.Size = new System.Drawing.Size(27, 22);
+            this.btnWatched.TabIndex = 5;
+            this.btnWatched.Text = "...";
+            this.toolTip.SetToolTip(this.btnWatched, "Choose the colour that represents the watched (played) text in the list facade.");
+            this.btnWatched.UseVisualStyleBackColor = true;
+            this.btnWatched.Click += new System.EventHandler(this.btnWatched_Click);
+            // 
+            // txtWatched
+            // 
+            this.txtWatched.BackColor = System.Drawing.Color.Black;
+            this.txtWatched.ForeColor = System.Drawing.Color.LightGray;
+            this.txtWatched.Location = new System.Drawing.Point(75, 49);
+            this.txtWatched.Name = "txtWatched";
+            this.txtWatched.Size = new System.Drawing.Size(49, 20);
+            this.txtWatched.TabIndex = 4;
+            this.txtWatched.Text = "808080";
+            this.txtWatched.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Watched:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnText
+            // 
+            this.btnText.Location = new System.Drawing.Point(130, 21);
+            this.btnText.Name = "btnText";
+            this.btnText.Size = new System.Drawing.Size(27, 22);
+            this.btnText.TabIndex = 2;
+            this.btnText.Text = "...";
+            this.toolTip.SetToolTip(this.btnText, "Choose the colour that represents the default text in the list facade, for videos" +
+                    " this\r\nis typically the Un-Watched colour.");
+            this.btnText.UseVisualStyleBackColor = true;
+            this.btnText.Click += new System.EventHandler(this.btnText_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtText.ForeColor = System.Drawing.Color.White;
+            this.txtText.Location = new System.Drawing.Point(75, 23);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(49, 20);
+            this.txtText.TabIndex = 1;
+            this.txtText.Text = "FFFFFF";
+            this.txtText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // toolTip
+            // 
+            this.toolTip.AutoPopDelay = 8000;
+            this.toolTip.InitialDelay = 500;
+            this.toolTip.IsBalloon = true;
+            this.toolTip.ReshowDelay = 100;
+            this.toolTip.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip.ToolTipTitle = "Colour Help";
+            // 
+            // ListColorDialog
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 197);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ListColorDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List Colours";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnText3;
+        private System.Windows.Forms.TextBox txtText3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnText2;
+        private System.Windows.Forms.TextBox txtText2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRemote;
+        private System.Windows.Forms.TextBox txtRemote;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnWatched;
+        private System.Windows.Forms.TextBox txtWatched;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnText;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
