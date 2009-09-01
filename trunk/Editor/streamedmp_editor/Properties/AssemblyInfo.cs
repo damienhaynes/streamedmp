d31e0bfe692b47332523190d6aedd2b12febd648
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2009-09-01 07:07:37 +0000

    Issue 179: Fix BasicHome when no RSS Feed is enabled or defined. I removed the #rssseparator property and hard coded with '::'
    Updated RSSTicker so that there is a default feed if none is defined. Fixed visibility of RSS background when there is no feed items or RSS is disabled.

--- trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
+++ trunk/Editor/streamedmp_editor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.569.0")]
-[assembly: AssemblyFileVersion("0.9.569.0")]
+[assembly: AssemblyVersion("0.9.595.0")]
+[assembly: AssemblyFileVersion("0.9.595.0")]
