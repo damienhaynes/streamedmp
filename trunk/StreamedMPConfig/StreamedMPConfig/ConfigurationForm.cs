aa92b3a5f2404acc076abff985c53a5d50954e20
tdnemail@gmail.com <tdnemail@gmail.com@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-05-12 20:31:40 +0000

    Conversion to windows plugin + skin file and changes to settings xmls to allow access from the settings menu..
    Todo : Commit the music/video screens that use the properties.
           localisation

--- trunk/StreamedMPConfig/StreamedMPConfig/ConfigurationForm.cs
+++ trunk/StreamedMPConfig/StreamedMPConfig/ConfigurationForm.cs
@@ -26,13 +26,10 @@ namespace StreamedMPConfig
         // Save settings to file
         private void btSave_Click(object sender, EventArgs e)
         {
-            using (MediaPortal.Profile.Settings xmlwriter = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
-            {
-                xmlwriter.SetValue("StreamedMPConfig", "cdCoverOnly", cbCdCoverOnly.Checked ? 1 : 0);
-                xmlwriter.SetValue("StreamedMPConfig", "showEqGraphic", cbShowEqGraphic.Checked ? 1 : 0);
-                xmlwriter.SetValue("StreamedMPConfig", "fullVideoOSD", fullVideoOSD.Checked ? 1 : 0);
-
-            }
+            StreamedMPConfig.cdCoverOnly = cbCdCoverOnly.Checked;
+            StreamedMPConfig.showEqGraphic = cbShowEqGraphic.Checked;
+            StreamedMPConfig.fullVideoOSD = fullVideoOSD.Checked;
+            settings.Save();
             this.Close();
         }
 
@@ -44,25 +41,19 @@ namespace StreamedMPConfig
         // Load settings from xml
         private void ConfigurationForm_Load(object sender, EventArgs e)
         {
-            using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
-            {
-                cbCdCoverOnly.Checked = true;
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "cdCoverOnly", 1) != 1)
-                    cbCdCoverOnly.Checked = false;
-                cbShowEqGraphic.Checked = true;
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "showEqGraphic", 1) != 1)
-                  cbShowEqGraphic.Checked = false;
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "fullVideoOSD", 1) == 1)
-                {
-                  fullVideoOSD.Checked = true;
-                  minVideoOSD.Checked = false;
-                }
-                else
-                {
-                  fullVideoOSD.Checked = false;
-                  minVideoOSD.Checked = true;
-                }
+            settings.Load();
+            cbCdCoverOnly.Checked = StreamedMPConfig.cdCoverOnly;
+            cbShowEqGraphic.Checked = StreamedMPConfig.showEqGraphic;
 
+            if (StreamedMPConfig.fullVideoOSD)
+            {
+                fullVideoOSD.Checked = true;
+                minVideoOSD.Checked = false;
+            }
+            else
+            {
+                fullVideoOSD.Checked = false;
+                minVideoOSD.Checked = true;
             }
         }
 
