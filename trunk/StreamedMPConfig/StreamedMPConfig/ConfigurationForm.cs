0ba79d4169325676d7dc5d28796dcf09402eb0c5
tdnemail@gmail.com <tdnemail@gmail.com@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-05-13 21:36:41 +0000

    Cosmetic code tidy. Change output directory to Plugins/windows

--- trunk/StreamedMPConfig/StreamedMPConfig/ConfigurationForm.cs
+++ trunk/StreamedMPConfig/StreamedMPConfig/ConfigurationForm.cs
@@ -11,67 +11,64 @@ using System.Reflection;
 
 namespace StreamedMPConfig
 {
-    public partial class ConfigurationForm : Form
+  public partial class ConfigurationForm : Form
+  {
+    public ConfigurationForm()
     {
-        public ConfigurationForm()
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            releaseVersion.Text = String.Format("Version: {0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
-            DateTime buildDate = getLinkerTimeStamp(System.Reflection.Assembly.GetEntryAssembly().Location.ToString());
-            compileTime.Text += " " + buildDate.ToString() + " GMT";
-
-        }
-
-        // Save settings to file
-        private void btSave_Click(object sender, EventArgs e)
-        {
-            StreamedMPConfig.cdCoverOnly = cbCdCoverOnly.Checked;
-            StreamedMPConfig.showEqGraphic = cbShowEqGraphic.Checked;
-            StreamedMPConfig.fullVideoOSD = fullVideoOSD.Checked;
-            settings.Save();
-            this.Close();
-        }
-
-        private void btCancel_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        // Load settings from xml
-        private void ConfigurationForm_Load(object sender, EventArgs e)
-        {
-            settings.Load();
-            cbCdCoverOnly.Checked = StreamedMPConfig.cdCoverOnly;
-            cbShowEqGraphic.Checked = StreamedMPConfig.showEqGraphic;
-
-            if (StreamedMPConfig.fullVideoOSD)
-            {
-                fullVideoOSD.Checked = true;
-                minVideoOSD.Checked = false;
-            }
-            else
-            {
-                fullVideoOSD.Checked = false;
-                minVideoOSD.Checked = true;
-            }
-        }
-
-        private static DateTime getLinkerTimeStamp(string filePath)
-        {
-          const int PeHeaderOffset = 60;
-          const int LinkerTimestampOffset = 8;
-
-          byte[] b = new byte[2047];
-          using (System.IO.Stream s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
-          {
-            s.Read(b, 0, 2047);
-          }
-
-          int secondsSince1970 = BitConverter.ToInt32(b, BitConverter.ToInt32(b, PeHeaderOffset) + LinkerTimestampOffset);
-
-          return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(secondsSince1970);
-        }
+      releaseVersion.Text = String.Format("Version: {0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+      DateTime buildDate = getLinkerTimeStamp(System.Reflection.Assembly.GetEntryAssembly().Location.ToString());
+      compileTime.Text += " " + buildDate.ToString() + " GMT";
 
     }
+
+    // Save settings to file
+    private void btSave_Click(object sender, EventArgs e)
+    {
+      StreamedMPConfig.cdCoverOnly = cbCdCoverOnly.Checked;
+      StreamedMPConfig.showEqGraphic = cbShowEqGraphic.Checked;
+      StreamedMPConfig.fullVideoOSD = fullVideoOSD.Checked;
+      settings.Save();
+      this.Close();
+    }
+
+    private void btCancel_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    // Load settings from xml
+    private void ConfigurationForm_Load(object sender, EventArgs e)
+    {
+      settings.Load();
+      cbCdCoverOnly.Checked = StreamedMPConfig.cdCoverOnly;
+      cbShowEqGraphic.Checked = StreamedMPConfig.showEqGraphic;
+
+      if (StreamedMPConfig.fullVideoOSD)
+      {
+        fullVideoOSD.Checked = true;
+        minVideoOSD.Checked = false;
+      }
+      else
+      {
+        fullVideoOSD.Checked = false;
+        minVideoOSD.Checked = true;
+      }
+    }
+
+    private static DateTime getLinkerTimeStamp(string filePath)
+    {
+      const int PeHeaderOffset = 60;
+      const int LinkerTimestampOffset = 8;
+
+      byte[] b = new byte[2047];
+      using (System.IO.Stream s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+      {
+        s.Read(b, 0, 2047);
+      }
+      int secondsSince1970 = BitConverter.ToInt32(b, BitConverter.ToInt32(b, PeHeaderOffset) + LinkerTimestampOffset);
+      return new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(secondsSince1970);
+    }
+  }
 }
--- trunk/StreamedMPConfig/StreamedMPConfig/settings.cs
+++ trunk/StreamedMPConfig/StreamedMPConfig/settings.cs
@@ -14,37 +14,35 @@ using MediaPortal.Util;
 
 namespace StreamedMPConfig
 {
-      internal static class settings
+  internal static class settings
+  {
+
+    public static void Load()
     {
+      Log.Info("StreamedMPConfig: Settings.Load()");
+      using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
+      {
+        // Read user settings from configuration file
 
-        public static void Load()
-        {
-            Log.Info("StreamedMPConfig: Settings.Load()");
-            using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
-            {
-                // Read user settings from configuration file
-
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "cdCoverOnly", 1) != 0)
-                    StreamedMPConfig.cdCoverOnly = true;
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "showEqGraphic", 1) != 0)
-                    StreamedMPConfig.showEqGraphic = true;
-                if (xmlreader.GetValueAsInt("StreamedMPConfig", "fullVideoOSD", 1) != 0)
-                    StreamedMPConfig.fullVideoOSD = true;
-             }
-        }
-
-        public static void Save()
-        {
-            Log.Info("StreamedMP: Settings.Save()");
-            using (MediaPortal.Profile.Settings xmlwriter = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
-            {
-                xmlwriter.SetValue("StreamedMPConfig", "cdCoverOnly", StreamedMPConfig.cdCoverOnly ? 1 : 0);
-                xmlwriter.SetValue("StreamedMPConfig", "showEqGraphic", StreamedMPConfig.showEqGraphic ? 1 : 0);
-                xmlwriter.SetValue("StreamedMPConfig", "fullVideoOSD", StreamedMPConfig.fullVideoOSD ? 1 : 0);
-
-            }
-        }
-
-
+        if (xmlreader.GetValueAsInt("StreamedMPConfig", "cdCoverOnly", 1) != 0)
+          StreamedMPConfig.cdCoverOnly = true;
+        if (xmlreader.GetValueAsInt("StreamedMPConfig", "showEqGraphic", 1) != 0)
+          StreamedMPConfig.showEqGraphic = true;
+        if (xmlreader.GetValueAsInt("StreamedMPConfig", "fullVideoOSD", 1) != 0)
+          StreamedMPConfig.fullVideoOSD = true;
+      }
     }
+
+    public static void Save()
+    {
+      Log.Info("StreamedMP: Settings.Save()");
+      using (MediaPortal.Profile.Settings xmlwriter = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "StreamedMPConfig.xml")))
+      {
+        xmlwriter.SetValue("StreamedMPConfig", "cdCoverOnly", StreamedMPConfig.cdCoverOnly ? 1 : 0);
+        xmlwriter.SetValue("StreamedMPConfig", "showEqGraphic", StreamedMPConfig.showEqGraphic ? 1 : 0);
+        xmlwriter.SetValue("StreamedMPConfig", "fullVideoOSD", StreamedMPConfig.fullVideoOSD ? 1 : 0);
+
+      }
+    }
+  }
 }
