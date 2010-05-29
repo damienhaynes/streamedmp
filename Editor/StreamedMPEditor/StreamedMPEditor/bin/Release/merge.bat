4e089380d8e8576ad3af7e2b068753439b3ce027
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-05-29 01:47:38 +0000

    SVN cleanup

--- Editor/StreamedMPEditor/StreamedMPEditor/bin/Release/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST StreamedMPEditor_UNMERGED.exe del StreamedMPEditor_UNMERGED.exe
-ren StreamedMPEditor.exe StreamedMPEditor_UNMERGED.exe
-ilmerge /out:StreamedMPEditor.exe StreamedMPEditor_UNMERGED.exe ICSharpCode.SharpZipLib.dll
