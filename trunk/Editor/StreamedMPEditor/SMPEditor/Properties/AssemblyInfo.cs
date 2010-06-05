00e5bf4c839bc442c4271397f1db062ec9a1bd21
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-06-05 00:09:19 +0000

    Updated version to v1.2.0.1410

--- trunk/Editor/StreamedMPEditor/SMPEditor/Properties/AssemblyInfo.cs
+++ trunk/Editor/StreamedMPEditor/SMPEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.1408")]
-[assembly: AssemblyFileVersion("1.2.0.1408")]
+[assembly: AssemblyVersion("1.2.0.1410")]
+[assembly: AssemblyFileVersion("1.2.0.1410")]
