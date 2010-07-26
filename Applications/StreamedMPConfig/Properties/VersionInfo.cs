bf5efb7d3e4cf61aa627a176f37d459414e19daf
tdnemail <tdnemail@googlemail.com>
2010-07-26 11:39:28 +0000

    Removed the reference patch.

--- Applications/StreamedMPConfig/Properties/VersionInfo.cs
+++ Applications/StreamedMPConfig/Properties/VersionInfo.cs
@@ -1,1 +1,1 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.2.1.1553")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.2.1.1565")]
--- Applications/StreamedMPEditor/Properties/VersionInfo.cs
+++ Applications/StreamedMPEditor/Properties/VersionInfo.cs
@@ -1,1 +1,1 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.2.1.1561")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.2.1.1565")]
