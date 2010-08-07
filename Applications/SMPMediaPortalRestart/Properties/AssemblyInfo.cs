c608fb874484ff5ec182e4e2d5bb1a4768529b6a
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-07 15:19:40 +0000

    Added versioning to SMPPatch and SMPMediaPortalRestart.

--- Applications/SMPMediaPortalRestart/Properties/AssemblyInfo.cs
+++ Applications/SMPMediaPortalRestart/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9a7f7ad2-3a3a-49e4-8d05-401b4464e527")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
