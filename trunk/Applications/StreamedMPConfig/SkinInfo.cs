32410911ba5d7b9c62c8f0cb6516832e06d6c168
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-07 08:27:13 +0000

    Patch can now copy files to database path, this is needed to patch FanartHandler default database file (FanartHandler.org).

--- trunk/Applications/StreamedMPConfig/SkinInfo.cs
+++ trunk/Applications/StreamedMPConfig/SkinInfo.cs
@@ -36,6 +36,7 @@ namespace StreamedMPConfig
       //public string backgroundPath;
       public string fanartBasePath;
       public string thumbsPath;
+      public string databasePath;
     }
 
     #endregion
@@ -166,6 +167,15 @@ namespace StreamedMPConfig
             if (path != null)
             {
                 mpPaths.langBasePath = GetMediaPortalDir(path.InnerText);
+            }
+        }
+        // get the Database base path
+        if (innerNode.InnerText == "Database")
+        {
+            XmlNode path = node.SelectSingleNode("Path");
+            if (path != null)
+            {
+                mpPaths.databasePath = GetMediaPortalDir(path.InnerText);
             }
         }
       }
