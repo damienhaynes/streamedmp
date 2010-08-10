448a929ddb8275234426fa1ee47b3357ee21ba64
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-10 03:49:59 +0000

    Removed the patch binaries from svn, now they are copied in a PreBuild step.

---
+++ trunk/Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
@@ -0,0 +1,7 @@
+rem Copying patch files Embedded Resources
+echo on
+copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
+copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
+copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
+copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
+copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
