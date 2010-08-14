11b3616830485cf64d8d87d677485ead4b8a9f02
tdnemail@gmail.com <tdnemail@gmail.com@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-14 23:50:34 +0000

    test program - this will calculate and add a checksum to all xml files in the live StreamedMP directory, in addition it reformats the files as it goes (saving 300k).
    I see this as the next stage with the patching but will  required a major release before it can be realised. If we checksum all xml files we have a method of checking if users have modified files and if they have we can not update these and provide a copy of the patched files elsewhere.
    
    The main part is a class that can be added to any program to add, read and compare the checksums on files.

---
+++ trunk/Trevor-checksum/SMPCheckSum/SMPCheckSum/SMPCheckSum.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SMPCheckSum
+{
+  partial class SMPCheckSum
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SMPCheckSum));
+      this.chkSumFiles = new System.Windows.Forms.ListView();
+      this.Filename = new System.Windows.Forms.ColumnHeader();
+      this.Checksum = new System.Windows.Forms.ColumnHeader();
+      this.btAddChksumToAll = new System.Windows.Forms.Button();
+      this.btVerifyChksum = new System.Windows.Forms.Button();
+      this.ilCheckSum = new System.Windows.Forms.ImageList(this.components);
+      this.pbCheckSum = new System.Windows.Forms.ProgressBar();
+      this.SuspendLayout();
+      // 
+      // chkSumFiles
+      // 
+      this.chkSumFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename,
+            this.Checksum});
+      this.chkSumFiles.Location = new System.Drawing.Point(122, 12);
+      this.chkSumFiles.Name = "chkSumFiles";
+      this.chkSumFiles.Size = new System.Drawing.Size(681, 215);
+      this.chkSumFiles.SmallImageList = this.ilCheckSum;
+      this.chkSumFiles.TabIndex = 0;
+      this.chkSumFiles.UseCompatibleStateImageBehavior = false;
+      this.chkSumFiles.View = System.Windows.Forms.View.Details;
+      this.chkSumFiles.SelectedIndexChanged += new System.EventHandler(this.chkSumFiles_SelectedIndexChanged);
+      // 
+      // Filename
+      // 
+      this.Filename.Text = "FileName";
+      this.Filename.Width = 284;
+      // 
+      // Checksum
+      // 
+      this.Checksum.Text = "Checksum";
+      this.Checksum.Width = 393;
+      // 
+      // btAddChksumToAll
+      // 
+      this.btAddChksumToAll.Location = new System.Drawing.Point(122, 264);
+      this.btAddChksumToAll.Name = "btAddChksumToAll";
+      this.btAddChksumToAll.Size = new System.Drawing.Size(179, 23);
+      this.btAddChksumToAll.TabIndex = 1;
+      this.btAddChksumToAll.Text = "Add/Display checksum on all files";
+      this.btAddChksumToAll.UseVisualStyleBackColor = true;
+      this.btAddChksumToAll.Click += new System.EventHandler(this.btAddChksumToAll_Click);
+      // 
+      // btVerifyChksum
+      // 
+      this.btVerifyChksum.Location = new System.Drawing.Point(616, 264);
+      this.btVerifyChksum.Name = "btVerifyChksum";
+      this.btVerifyChksum.Size = new System.Drawing.Size(187, 23);
+      this.btVerifyChksum.TabIndex = 2;
+      this.btVerifyChksum.Text = "Verify Checksum on Selected File";
+      this.btVerifyChksum.UseVisualStyleBackColor = true;
+      this.btVerifyChksum.Click += new System.EventHandler(this.btVerifyChksum_Click);
+      // 
+      // ilCheckSum
+      // 
+      this.ilCheckSum.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilCheckSum.ImageStream")));
+      this.ilCheckSum.TransparentColor = System.Drawing.Color.Transparent;
+      this.ilCheckSum.Images.SetKeyName(0, "checkSumOk.png");
+      this.ilCheckSum.Images.SetKeyName(1, "checkSumFailed.png");
+      // 
+      // pbCheckSum
+      // 
+      this.pbCheckSum.Location = new System.Drawing.Point(123, 235);
+      this.pbCheckSum.Name = "pbCheckSum";
+      this.pbCheckSum.Size = new System.Drawing.Size(679, 20);
+      this.pbCheckSum.TabIndex = 3;
+      // 
+      // SMPCheckSum
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(924, 318);
+      this.Controls.Add(this.pbCheckSum);
+      this.Controls.Add(this.btVerifyChksum);
+      this.Controls.Add(this.btAddChksumToAll);
+      this.Controls.Add(this.chkSumFiles);
+      this.Name = "SMPCheckSum";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.SMPCheckSum_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListView chkSumFiles;
+    private System.Windows.Forms.ColumnHeader Filename;
+    private System.Windows.Forms.ColumnHeader Checksum;
+    private System.Windows.Forms.Button btAddChksumToAll;
+    private System.Windows.Forms.Button btVerifyChksum;
+    private System.Windows.Forms.ImageList ilCheckSum;
+    private System.Windows.Forms.ProgressBar pbCheckSum;
+  }
+}
+
---
+++ trunk/Trevor-checksum/SMPCheckSum/SMPCheckSum/SMPCheckSum.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+
+namespace SMPCheckSum
+{
+  public partial class SMPCheckSum : Form
+  {
+    SkinInfo skInfo = new SkinInfo();
+    string[] skinFiles;
+    string xml;
+    CheckSum checkSum = new CheckSum();
+
+
+    public SMPCheckSum()
+    {
+      InitializeComponent();
+    }
+
+
+    private void SMPCheckSum_Load(object sender, EventArgs e)
+    {
+      btVerifyChksum.Enabled = false;
+    }
+
+    
+    private void btAddChksumToAll_Click(object sender, EventArgs e)
+    {
+      btVerifyChksum.Enabled = false;
+
+      DirectoryInfo dInfo = new DirectoryInfo(SkinInfo.mpPaths.streamedMPpath);
+      pbCheckSum.Maximum = dInfo.GetFiles("*.xml").Length;
+
+      skinFiles = Directory.GetFiles(SkinInfo.mpPaths.streamedMPpath, "*.xml");
+      skinFile fileToCheck = new skinFile();
+
+      chkSumFiles.Items.Clear();
+      
+      foreach (string chkFile in skinFiles)
+      {
+
+        fileToCheck.pathAndFilename = chkFile;
+        fileToCheck.fileName = Path.GetFileName(chkFile);
+        if (checkSum.Read(chkFile) == null)
+          fileToCheck.checkSum = checkSum.Add(chkFile);
+        else
+          fileToCheck.checkSum = checkSum.Read(chkFile);
+
+        ListViewItem item = new ListViewItem(new[] { fileToCheck.fileName, fileToCheck.checkSum });
+        if (checkSum.Compare(chkFile))
+          item.ImageIndex = 0;
+        else
+          item.ImageIndex = 1;
+
+        chkSumFiles.Items.Add(item);
+        pbCheckSum.Value++;
+      }
+    }
+
+
+
+    private void btVerifyChksum_Click(object sender, EventArgs e)
+    {
+      if (!checkSum.Compare(Path.Combine(SkinInfo.mpPaths.streamedMPpath, chkSumFiles.SelectedItems[0].Text)))
+        chkSumFiles.SelectedItems[0].ImageIndex = 1;
+      else
+        chkSumFiles.SelectedItems[0].ImageIndex = 0;
+      chkSumFiles.Refresh();
+    }
+
+    public class skinFile
+    {
+      public string pathAndFilename { get; set; }
+      public string fileName { get; set; }
+      public string checkSum { get; set; }
+    }
+
+    private void chkSumFiles_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      btVerifyChksum.Enabled = true;
+    }
+
+  }
+}
