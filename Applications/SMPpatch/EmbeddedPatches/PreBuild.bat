d540087867a3afad7feb3eeb3f2c31e06ad7e78e
tdnemail <tdnemail@gmail.com>
2010-08-23 19:25:19 +0000

    Removed embedded files from patch and rem all copy commands - have not cleaned the PatchControl.xml

--- Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
+++ Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
@@ -1,10 +1,10 @@
 rem Copying patch files Embedded Resources
 rem Just comment out files that are not needed in patch
-copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
-copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
-copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
-copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
-copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
-copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
-copy ..\..\..\PluginDependencies\LyricsEngine.dll .\ /Y
-copy ..\..\..\PluginDependencies\TranslateProvider.dll .\ /Y
+rem copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
+rem copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
+rem copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
+rem copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
+rem copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
+rem copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
+rem copy ..\..\..\PluginDependencies\LyricsEngine.dll .\ /Y
+rem copy ..\..\..\PluginDependencies\TranslateProvider.dll .\ /Y
