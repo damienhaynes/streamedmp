5382d382f427aa12ad691dd11713ee2aa52d4b91
tdnemail@gmail.com <tdnemail@gmail.com@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-28 10:33:26 +0000

    Added back SMPCheckSum.dll

--- trunk/Applications/SMPpatch/ILMerge/merge.bat
+++ trunk/Applications/SMPpatch/ILMerge/merge.bat
@@ -1,4 +1,4 @@
 @echo off
 IF EXIST SMPPatch_UNMERGED.exe del SMPPatch_UNMERGED.exe
 ren SMPpatch.exe SMPPatch_UNMERGED.exe
-ilmerge /out:SMPPatch.exe SMPPatch_UNMERGED.exe ICSharpCode.SharpZipLib.dll
+ilmerge /out:SMPPatch.exe SMPPatch_UNMERGED.exe ICSharpCode.SharpZipLib.dll SMPCheckSum.dll
