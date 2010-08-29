c9252c61de3f30416d9740a38ad00e8cfd5421c7
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-08-29 05:58:03 +0000

    Handle FileNotFound exceptions when copying new xml files via patch. Added check so checksum compare is only done on skin xml files.

--- trunk/Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
+++ trunk/Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
@@ -1,10 +1,14 @@
 rem Copying patch files Embedded Resources
 rem Just comment out files that are not needed in patch
 rem copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
-rem copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
-rem copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
+copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
+copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
+copy ..\..\..\Plugins\windows\MovingPictures.dll .\ /Y
+copy ..\..\..\Plugins\windows\Cornerstone.dll .\ /Y
+copy ..\..\..\Plugins\windows\Cornerstone.MP.dll .\ /Y
 rem copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
-rem copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
-rem copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
+copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
+copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
+copy ..\..\..\PluginDependencies\SMPCheckSum.dll .\ /Y
 rem copy ..\..\..\PluginDependencies\LyricsEngine.dll .\ /Y
 rem copy ..\..\..\PluginDependencies\TranslateProvider.dll .\ /Y
