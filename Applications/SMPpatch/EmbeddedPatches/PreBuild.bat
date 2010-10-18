66efc988797b02426ba522f3d61fb152cb53da28
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-10-18 05:31:16 +0000

    Updated project for next patch.

--- Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
+++ Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
@@ -1,16 +1,16 @@
 rem Copying patch files Embedded Resources
 rem Just comment out files that are not needed in patch
-rem copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
+copy ..\..\..\Plugins\process\FanartHandler.dll .\ /Y
 copy ..\..\..\Plugins\process\StreamedMPEditor.dll .\ /Y
 copy ..\..\..\Plugins\windows\StreamedMPConfig.dll .\ /Y
 copy ..\..\..\Plugins\windows\MP-TVSeries.dll .\ /Y
-copy ..\..\..\Plugins\windows\MovingPictures.dll .\ /Y
-copy ..\..\..\Plugins\windows\Cornerstone.dll .\ /Y
-copy ..\..\..\Plugins\windows\Cornerstone.MP.dll .\ /Y
-rem copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
+rem copy ..\..\..\Plugins\windows\MovingPictures.dll .\ /Y
+rem copy ..\..\..\Plugins\windows\Cornerstone.dll .\ /Y
+rem copy ..\..\..\Plugins\windows\Cornerstone.MP.dll .\ /Y
+copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
 copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
 copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
 copy ..\..\..\PluginDependencies\SMPCheckSum.dll .\ /Y
-copy ..\..\..\PluginDependencies\SubtitleDownloader.dll .\ /Y
-rem copy ..\..\..\PluginDependencies\LyricsEngine.dll .\ /Y
-rem copy ..\..\..\PluginDependencies\TranslateProvider.dll .\ /Y
+rem copy ..\..\..\PluginDependencies\SubtitleDownloader.dll .\ /Y
+copy ..\..\..\PluginDependencies\LyricsEngine.dll .\ /Y
+copy ..\..\..\PluginDependencies\TranslateProvider.dll .\ /Y
