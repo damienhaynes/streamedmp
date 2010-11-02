1855e5dd7ac7f95d3942f1f14e086ac9adcea2ee
tdnemail <tdnemail@gmail.com>
2010-11-02 12:26:22 +0000

    Hide new most recent enable options prior to next release.

---
+++ Applications/StreamedMPEditor/SelectHardDrives.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace StreamedMPEditor
+{
+    partial class SelectHardDrives
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SelectHardDrives
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 263);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "SelectHardDrives";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Free Drive Space Overlay - Drive Selection";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
