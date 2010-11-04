b67aabc5f309fc70c01a478dd049570d35e0c9fa
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-11-04 02:27:31 +0000

    Commit missing source files.

---
+++ Applications/StreamedMPConfig/RecentlyAdded.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaPortal.Plugins.MovingPictures.Database;
+using WindowPlugins.GUITVSeries;
+
+namespace StreamedMPConfig
+{
+  public class RecentlyAdded
+  {
+    public static List<DBMovieInfo> recentAddedMovies = null;
+    public static List<DBEpisode> recentAddedEpisodes = null;
+  }
+}
---
+++ Applications/StreamedMPConfig/VideoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaPortal.Plugins.MovingPictures.MainUI;
+using WindowPlugins.GUITVSeries;
+
+namespace StreamedMPConfig
+{
+  public class VideoPlayer
+  {
+    public static MoviePlayer moviePlayer = null;
+    public static VideoHandler episodePlayer = null;
+  }
+}
