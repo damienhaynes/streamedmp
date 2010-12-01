9c6b1c89baf4ec6df1e7ad0d0dc41e29f6251627
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-12-01 07:48:14 +0000

    Commit missing file.

---
+++ trunk/Applications/StreamedMPConfig/PostPatchUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamedMPConfig
+{
+  class PostPatchUpdate
+  {
+    public static void UpdateSettings()
+    {
+      // update references.xml with user settings
+      MiscConfigGUI.SetColors();
+      MiscConfigGUI.SetAlphaTransparency();
+
+      // Set Skin Imports
+      TVSeriesConfig.ApplyConfigurationChanges();
+      MovingPicturesConfig.ApplyConfigurationChanges();
+      TVConfig.SetTVGuideSize();
+      TVConfig.SetRandomFanartProperties();
+      MusicOptionsGUI.SetMusicNowPlayingStyle();
+    }
+
+  }
+}
