d525f79a90bb70da532a5a4ba756561ca01a7ac4
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2010-12-29 22:08:17 +0000

    [MP 1.2] Removed ICSharpCode.SharpZipLib.dll library from installation, now included in MP distribution.
    Note: SMPPatch will still merge dll as that runs from different directory. Added ICSharpCode to externals folder.

--- Applications/SMPEditor/ILMerge/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST SMPEditor_UNMERGED.exe del SMPEditor_UNMERGED.exe
-ren SMPEditor.exe SMPEditor_UNMERGED.exe
-ilmerge /out:SMPEditor.exe SMPEditor_UNMERGED.exe StreamedMPEditor.dll SMPCheckSum.dll
