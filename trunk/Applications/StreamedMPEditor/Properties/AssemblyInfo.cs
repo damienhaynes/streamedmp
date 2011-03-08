802268c5bc44b6a2cc309b24f331af4767123c9d
tdnemail@gmail.com <tdnemail@gmail.com@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2011-03-08 14:03:58 +0000

    [MP 1.2] Add Plugin Compatibility code - not added any subsystem as works without, will check though the subsystems later

--- trunk/Applications/StreamedMPEditor/Properties/AssemblyInfo.cs
+++ trunk/Applications/StreamedMPEditor/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using MediaPortal.Common.Utils;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -22,3 +23,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("20efe727-8a7c-46e8-a526-a134e7a15a49")]
 
+[assembly: CompatibleVersion("1.1.7.0", "1.1.6.0")]
