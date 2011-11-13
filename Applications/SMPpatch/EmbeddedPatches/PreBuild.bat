5e656108372ca88ebc5134c0a371eeaa8864a578
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2011-11-13 03:49:03 +0000

    Commit binaries, versioning and patch definition as part of release.

--- Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
+++ Applications/SMPpatch/EmbeddedPatches/PreBuild.bat
@@ -8,6 +8,8 @@ copy ..\..\..\Plugins\windows\MovingPictures.dll .\ /Y
 copy ..\..\..\Plugins\windows\Cornerstone.dll .\ /Y
 copy ..\..\..\Plugins\windows\Cornerstone.MP.dll .\ /Y
 copy ..\..\..\Plugins\windows\MyLyrics.dll .\ /Y
+copy ..\..\..\Plugins\windows\GUIGlobalSearch.dll .\ /Y
+copy ..\..\..\Plugins\windows\InfoService.dll .\ /Y
 copy ..\..\SMPMediaPortalRestart\bin\Release\SMPMediaPortalRestart.exe .\ /Y
 copy ..\..\SMPEditor\bin\Release\SMPEditor.exe .\ /Y
 copy ..\..\..\PluginDependencies\SMPCheckSum.dll .\ /Y
