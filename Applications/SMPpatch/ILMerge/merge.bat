54618f82aff79d8821a07fa09813ada9ed30c81c
damien.haynes <damien.haynes@d8ae5aee-9d62-11dd-93a9-677c3c677ea2>
2014-02-21 13:13:09 +0000

    Updated ILMerge batch for SMPPatch.

--- Applications/SMPpatch/ILMerge/merge.bat
+++ Applications/SMPpatch/ILMerge/merge.bat
@@ -1,5 +1,18 @@
 @echo off
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    rem pause
+    rem echo Current path is %PROGS%	
+    goto CONT
+:32BIT
+    set PROGS=%ProgramFiles%
+    rem echo Current path is %PROGS%
+    rem pause
+:CONT
+
 IF EXIST SMPpatch_UNMERGED.exe del SMPpatch_UNMERGED.exe
 ren SMPpatch.exe SMPpatch_UNMERGED.exe
 rem ilmerge /out:SMPpatch.exe SMPpatch_UNMERGED.exe ICSharpCode.SharpZipLib.dll SMPCheckSum.dll
-ilmerge /out:SMPpatch.exe SMPpatch_UNMERGED.exe ICSharpCode.SharpZipLib.dll SMPCheckSum.dll /target:exe /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
+ilmerge /out:SMPpatch.exe SMPpatch_UNMERGED.exe ICSharpCode.SharpZipLib.dll SMPCheckSum.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
